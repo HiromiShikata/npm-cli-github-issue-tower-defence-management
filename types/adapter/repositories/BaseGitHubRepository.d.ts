fb997476985172c7db2352c8ca90b3f1df7ac999
HiromiShikata <HiromiShikata@users.noreply.github.com>
2025-01-04 05:23:09 +0000

    style: `npm run fmt`

--- types/adapter/repositories/BaseGitHubRepository.d.ts
+++ types/adapter/repositories/BaseGitHubRepository.d.ts
@@ -1,34 +1,43 @@
 import { LocalStorageRepository } from './LocalStorageRepository';
 interface Cookie {
-    name: string;
-    value: string;
-    domain?: string;
-    path?: string;
-    expires?: number;
-    httpOnly?: boolean;
-    secure?: boolean;
-    sameSite?: 'lax' | 'strict' | 'none';
+  name: string;
+  value: string;
+  domain?: string;
+  path?: string;
+  expires?: number;
+  httpOnly?: boolean;
+  secure?: boolean;
+  sameSite?: 'lax' | 'strict' | 'none';
 }
 export declare class BaseGitHubRepository {
-    readonly localStorageRepository: LocalStorageRepository;
-    readonly jsonFilePath: string;
-    readonly ghToken: string;
-    readonly ghUserName: string | undefined;
-    readonly ghUserPassword: string | undefined;
-    readonly ghAuthenticatorKey: string | undefined;
-    cookie: string | null;
-    constructor(localStorageRepository: LocalStorageRepository, jsonFilePath?: string, ghToken?: string, ghUserName?: string | undefined, ghUserPassword?: string | undefined, ghAuthenticatorKey?: string | undefined);
-    protected extractIssueFromUrl: (issueUrl: string) => {
-        owner: string;
-        repo: string;
-        issueNumber: number;
-        isIssue: boolean;
-    };
-    getCookie: () => Promise<string>;
-    createHeader: () => Promise<object>;
-    protected createCookieStringFromFile: () => Promise<string>;
-    protected isCookie: (cookie: object) => cookie is Cookie;
-    protected generateCookieHeaderFromJson: (cookieData: unknown) => Promise<string>;
+  readonly localStorageRepository: LocalStorageRepository;
+  readonly jsonFilePath: string;
+  readonly ghToken: string;
+  readonly ghUserName: string | undefined;
+  readonly ghUserPassword: string | undefined;
+  readonly ghAuthenticatorKey: string | undefined;
+  cookie: string | null;
+  constructor(
+    localStorageRepository: LocalStorageRepository,
+    jsonFilePath?: string,
+    ghToken?: string,
+    ghUserName?: string | undefined,
+    ghUserPassword?: string | undefined,
+    ghAuthenticatorKey?: string | undefined,
+  );
+  protected extractIssueFromUrl: (issueUrl: string) => {
+    owner: string;
+    repo: string;
+    issueNumber: number;
+    isIssue: boolean;
+  };
+  getCookie: () => Promise<string>;
+  createHeader: () => Promise<object>;
+  protected createCookieStringFromFile: () => Promise<string>;
+  protected isCookie: (cookie: object) => cookie is Cookie;
+  protected generateCookieHeaderFromJson: (
+    cookieData: unknown,
+  ) => Promise<string>;
 }
 export {};
 //# sourceMappingURL=BaseGitHubRepository.d.ts.map
--- types/domain/usecases/AnalyzeStoriesUseCase.d.ts
+++ types/domain/usecases/AnalyzeStoriesUseCase.d.ts
@@ -5,25 +5,34 @@ import { Member } from '../entities/Member';
 import { DateRepository } from './adapter-interfaces/DateRepository';
 import { StoryObjectMap } from './HandleScheduledEventUseCase';
 export declare class AnalyzeStoriesUseCase {
-    readonly issueRepository: Pick<IssueRepository, 'createNewIssue'>;
-    readonly dateRepository: Pick<DateRepository, 'formatDurationToHHMM'>;
-    constructor(issueRepository: Pick<IssueRepository, 'createNewIssue'>, dateRepository: Pick<DateRepository, 'formatDurationToHHMM'>);
-    run: (input: {
-        targetDates: Date[];
-        project: Project;
-        issues: Issue[];
-        cacheUsed: boolean;
-        manager: Member["name"];
-        org: string;
-        repo: string;
-        urlOfStoryView: string;
-        disabledStatus: string;
-        storyObjectMap: StoryObjectMap;
-    }) => Promise<void>;
-    createSummaryIssueBody: (summaryStoryIssue: Map<string, (Issue & {
+  readonly issueRepository: Pick<IssueRepository, 'createNewIssue'>;
+  readonly dateRepository: Pick<DateRepository, 'formatDurationToHHMM'>;
+  constructor(
+    issueRepository: Pick<IssueRepository, 'createNewIssue'>,
+    dateRepository: Pick<DateRepository, 'formatDurationToHHMM'>,
+  );
+  run: (input: {
+    targetDates: Date[];
+    project: Project;
+    issues: Issue[];
+    cacheUsed: boolean;
+    manager: Member['name'];
+    org: string;
+    repo: string;
+    urlOfStoryView: string;
+    disabledStatus: string;
+    storyObjectMap: StoryObjectMap;
+  }) => Promise<void>;
+  createSummaryIssueBody: (
+    summaryStoryIssue: Map<
+      string,
+      (Issue & {
         name: string;
         color: string;
         description: string;
-    })[]>, urlOfStoryView: string) => string;
+      })[]
+    >,
+    urlOfStoryView: string,
+  ) => string;
 }
 //# sourceMappingURL=AnalyzeStoriesUseCase.d.ts.map
