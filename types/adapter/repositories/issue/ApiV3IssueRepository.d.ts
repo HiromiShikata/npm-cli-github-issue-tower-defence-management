d278f64514d36e7335dd9515b747304ba24084e0
gh-actions <gh-actions>
2025-01-06 04:57:32 +0000

    autogen: release

--- types/adapter/repositories/issue/ApiV3IssueRepository.d.ts
+++ types/adapter/repositories/issue/ApiV3IssueRepository.d.ts
@@ -1,19 +1,17 @@
 import { BaseGitHubRepository } from '../BaseGitHubRepository';
 export declare class ApiV3IssueRepository extends BaseGitHubRepository {
-  searchIssue: (query: {
-    owner: string;
-    repositoryName: string;
-    type?: 'issue' | 'pr';
-    state?: 'open' | 'closed' | 'all';
-    title?: string;
-    createdFrom?: string;
-    assignee?: string;
-  }) => Promise<
-    {
-      url: string;
-      title: string;
-      number: string;
-    }[]
-  >;
+    searchIssue: (query: {
+        owner: string;
+        repositoryName: string;
+        type?: "issue" | "pr";
+        state?: "open" | "closed" | "all";
+        title?: string;
+        createdFrom?: string;
+        assignee?: string;
+    }) => Promise<{
+        url: string;
+        title: string;
+        number: string;
+    }[]>;
 }
 //# sourceMappingURL=ApiV3IssueRepository.d.ts.map
--- types/domain/usecases/ChangeStatusLongInReviewIssueUseCase.d.ts
+++ types/domain/usecases/ChangeStatusLongInReviewIssueUseCase.d.ts
@@ -3,21 +3,15 @@ import { IssueRepository } from './adapter-interfaces/IssueRepository';
 import { Project } from '../entities/Project';
 import { DateRepository } from './adapter-interfaces/DateRepository';
 export declare class ChangeStatusLongInReviewIssueUseCase {
-  readonly dateRepository: Pick<DateRepository, 'now'>;
-  readonly issueRepository: Pick<
-    IssueRepository,
-    'updateStatus' | 'createComment'
-  >;
-  constructor(
-    dateRepository: Pick<DateRepository, 'now'>,
-    issueRepository: Pick<IssueRepository, 'updateStatus' | 'createComment'>,
-  );
-  run: (input: {
-    project: Project;
-    issues: Issue[];
-    cacheUsed: boolean;
-    org: string;
-    repo: string;
-  }) => Promise<void>;
+    readonly dateRepository: Pick<DateRepository, 'now'>;
+    readonly issueRepository: Pick<IssueRepository, 'updateStatus' | 'createComment'>;
+    constructor(dateRepository: Pick<DateRepository, 'now'>, issueRepository: Pick<IssueRepository, 'updateStatus' | 'createComment'>);
+    run: (input: {
+        project: Project;
+        issues: Issue[];
+        cacheUsed: boolean;
+        org: string;
+        repo: string;
+    }) => Promise<void>;
 }
 //# sourceMappingURL=ChangeStatusLongInReviewIssueUseCase.d.ts.map
