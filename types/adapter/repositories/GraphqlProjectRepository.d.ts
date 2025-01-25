a70fb824d5a56a083016351b8c40d1e8ffcbbeb9
HiromiShikata <6440811+HiromiShikata@users.noreply.github.com>
2025-01-25 08:02:30 +0000

    style: `npm run fmt`

--- types/adapter/repositories/GraphqlProjectRepository.d.ts
+++ types/adapter/repositories/GraphqlProjectRepository.d.ts
@@ -1,13 +1,16 @@
 import { BaseGitHubRepository } from './BaseGitHubRepository';
 import { ProjectRepository } from '../../domain/usecases/adapter-interfaces/ProjectRepository';
 import { Project } from '../../domain/entities/Project';
-export declare class GraphqlProjectRepository extends BaseGitHubRepository implements ProjectRepository {
-    extractProjectFromUrl: (projectUrl: string) => {
-        owner: string;
-        projectNumber: number;
-    };
-    fetchProjectId: (login: string, projectNumber: number) => Promise<string>;
-    findProjectIdByUrl: (projectUrl: string) => Promise<Project["id"] | null>;
-    getProject: (projectId: Project["id"]) => Promise<Project | null>;
+export declare class GraphqlProjectRepository
+  extends BaseGitHubRepository
+  implements ProjectRepository
+{
+  extractProjectFromUrl: (projectUrl: string) => {
+    owner: string;
+    projectNumber: number;
+  };
+  fetchProjectId: (login: string, projectNumber: number) => Promise<string>;
+  findProjectIdByUrl: (projectUrl: string) => Promise<Project['id'] | null>;
+  getProject: (projectId: Project['id']) => Promise<Project | null>;
 }
 //# sourceMappingURL=GraphqlProjectRepository.d.ts.map
