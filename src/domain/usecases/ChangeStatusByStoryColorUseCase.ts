36efb4fd3ee997da14efa6c9a50ffcb2d69512d7
HiromiShikata <shikata.hiromi@gmail.com>
2025-02-23 15:49:59 +0000

    prep: create ChangeStatusByStoryColorUseCase

---
+++ src/domain/usecases/ChangeStatusByStoryColorUseCase.ts
@@ -0,0 +1,62 @@
+import { IssueRepository } from './adapter-interfaces/IssueRepository';
+import { Project } from '../entities/Project';
+import { DateRepository } from './adapter-interfaces/DateRepository';
+import { StoryObjectMap } from './HandleScheduledEventUseCase';
+
+export class ChangeStatusByStoryColorUseCase {
+  constructor(
+    readonly dateRepository: Pick<DateRepository, 'now'>,
+    readonly issueRepository: Pick<
+      IssueRepository,
+      'updateStatus' | 'createComment'
+    >,
+  ) {}
+
+  run = async (input: {
+    project: Project;
+    cacheUsed: boolean;
+    org: string;
+    repo: string;
+    disabledStatus: string;
+    storyObjectMap: StoryObjectMap;
+  }): Promise<void> => {
+    const firstStatus = input.project.status.statuses[0];
+    if (!firstStatus) {
+      throw new Error('First status is not found');
+    } else if (input.cacheUsed) {
+      return;
+    }
+    for (const storyObject of Array.from(input.storyObjectMap.values())) {
+      const isStoryDisabled = storyObject.story.color === 'GRAY';
+      for (const issue of storyObject.issues) {
+        if (isStoryDisabled) {
+          if (issue.status && issue.status === input.disabledStatus) {
+            continue;
+          }
+          await this.issueRepository.updateStatus(
+            input.project,
+            issue,
+            input.disabledStatus,
+          );
+          await this.issueRepository.createComment(
+            issue,
+            `This issue status is changed because the story is disabled.`,
+          );
+        } else if (!isStoryDisabled) {
+          if (issue.status && issue.status !== input.disabledStatus) {
+            continue;
+          }
+          await this.issueRepository.updateStatus(
+            input.project,
+            issue,
+            firstStatus.id,
+          );
+          await this.issueRepository.createComment(
+            issue,
+            `This issue status is changed because the story is enabled.`,
+          );
+        }
+      }
+    }
+  };
+}
