0d0462b93b82066c3648de0630a65f0421fd80e5
gh-actions <gh-actions>
2026-03-27 05:55:55 +0000

    autogen: release

--- types/adapter/repositories/utils.d.ts
+++ types/adapter/repositories/utils.d.ts
@@ -1,4 +1,5 @@
-export declare const calcrateDuration: (start: string, end: string) => string;
+export declare const calcrateDuration: (start: string, // HH:mm
+end: string) => string;
 export declare const totalDuration: (durations: string[]) => string;
 export declare const normalizeFieldName: (fieldName: string) => string;
 //# sourceMappingURL=utils.d.ts.map
