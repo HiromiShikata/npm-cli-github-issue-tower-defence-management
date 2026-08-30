54fd9b04c4da46a793bb62964e1d4894e94ce06a
HiromiShikata <shikata.hiromi@gmail.com>
2026-08-30 16:03:32 +0000

    fix(dashboard): update adapter tests to match slimmed dashboard format
    Update byte-identical expected strings in dashboardComposeService,
    dashboardEmitterComposerKey, and webServer tests to reflect:
    - 2-digit project column widths with compact headers (td/qc/fl/pp/ws/dp)
    - token names showing last 2 characters without percent sign
    - session total line above token rows

--- src/adapter/entry-points/console/dashboardComposeService.test.ts
+++ src/adapter/entry-points/console/dashboardComposeService.test.ts
@@ -443,11 +443,12 @@ describe('composeDashboardText', () => {
       ).toBe(
         '<tt>M55%&nbsp;C62%&nbsp;D89%&nbsp;cy14</tt><br>\n' +
           '<tt>LA&nbsp;16&nbsp;23&nbsp;40</tt><br>\n' +
-          '<tt>pj&nbsp;&nbsp;&nbsp;tdo&nbsp;aqc&nbsp;fal&nbsp;prp&nbsp;aws&nbsp;dep&nbsp;🔴&nbsp;🟡&nbsp;🔵</tt><br>\n' +
-          '<tt>🟢ac&nbsp;&nbsp;&nbsp;1&nbsp;&nbsp;&nbsp;2&nbsp;&nbsp;&nbsp;0&nbsp;&nbsp;&nbsp;0&nbsp;&nbsp;&nbsp;4&nbsp;&nbsp;&nbsp;1&nbsp;&nbsp;0&nbsp;&nbsp;0&nbsp;&nbsp;0</tt><br>\n' +
-          '<tt>&nbsp;&nbsp;in&nbsp;&nbsp;--&nbsp;&nbsp;--&nbsp;&nbsp;--&nbsp;&nbsp;--&nbsp;&nbsp;--&nbsp;&nbsp;--&nbsp;--&nbsp;--&nbsp;--</tt><br>\n' +
+          '<tt>pj&nbsp;&nbsp;&nbsp;td&nbsp;qc&nbsp;fl&nbsp;pp&nbsp;ws&nbsp;dp&nbsp;🔴&nbsp;🟡&nbsp;🔵</tt><br>\n' +
+          '<tt>🟢ac&nbsp;&nbsp;1&nbsp;&nbsp;2&nbsp;&nbsp;0&nbsp;&nbsp;0&nbsp;&nbsp;4&nbsp;&nbsp;1&nbsp;&nbsp;0&nbsp;&nbsp;0&nbsp;&nbsp;0</tt><br>\n' +
+          '<tt>&nbsp;&nbsp;in&nbsp;--&nbsp;--&nbsp;--&nbsp;--&nbsp;--&nbsp;--&nbsp;--&nbsp;--&nbsp;--</tt><br>\n' +
           '<tt></tt><br>\n' +
-          '<tt>🟢alice&nbsp;&nbsp;10%&nbsp;0d01h00&nbsp;&nbsp;12%&nbsp;5d00h00&nbsp;2&nbsp;1</tt><br>\n',
+          '<tt>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2&nbsp;1</tt><br>\n' +
+          '<tt>🟢ce&nbsp;10&nbsp;0d01h00&nbsp;12&nbsp;5d00h00&nbsp;2&nbsp;1</tt><br>\n',
       );
     } finally {
       fs.rmSync(dataDir, { recursive: true, force: true });
