63a71aa3dcf6d4c84f577694a0855a68b0502610
HiromiShikata <shikata.hiromi@gmail.com>
2026-09-09 00:09:02 +0000

    fix(console): add missing description field to layout storybook fixtures

--- src/adapter/entry-points/console/ui/src/features/console/components/layout/IssueCreateModalDialog.stories.tsx
+++ src/adapter/entry-points/console/ui/src/features/console/components/layout/IssueCreateModalDialog.stories.tsx
@@ -7,6 +7,7 @@ const storyEntries: ConsoleStoryEntry[] = [
     storyName: 'regular / workflow improvement',
     storyOptionId: 'opt-workflow-improvement',
     color: 'BLUE',
+    description: '',
     openItemCount: 3,
     storyViewUrl:
       'https://github.com/users/HiromiShikata/projects/48/views/1?filterQuery=story%3A%22regular+%2F+workflow+improvement%22',
@@ -16,6 +17,7 @@ const storyEntries: ConsoleStoryEntry[] = [
     storyName: 'regular / tdpm dashboard & console improvement',
     storyOptionId: 'opt-tdpm-console',
     color: 'GREEN',
+    description: '',
     openItemCount: 5,
     storyViewUrl:
       'https://github.com/users/HiromiShikata/projects/48/views/1?filterQuery=story%3A%22regular+%2F+tdpm+dashboard+%26+console+improvement%22',
@@ -25,6 +27,7 @@ const storyEntries: ConsoleStoryEntry[] = [
     storyName: 'regular / infrastructure maintenance',
     storyOptionId: 'opt-infra',
     color: 'YELLOW',
+    description: '',
     openItemCount: 1,
     storyViewUrl: null,
     items: [],
